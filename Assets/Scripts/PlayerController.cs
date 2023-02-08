72419b298b8c060c5d4fd3bc81a850ae4a595742
Steve Rector <steverector27@gmail.com>
2023-02-08 13:09:10 -0800

    Add CharacterController component to player
    Also unparents player from camera, camera now just follows the position and rotation of the viewpoint game object

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,23 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement Configs")]
     [SerializeField] float moveSpeed;
-    [SerializeField] float rotationSpeed;
-    [SerializeField] float mouseSensitivity = 1f;
+    [SerializeField] float runSpeed;
+
+    [Header("Look Rotation Configs")]
     [SerializeField] Transform viewPoint;
+    [SerializeField] float rotationSpeed;
     [SerializeField] float lookRotationLimit;
     [SerializeField] bool invertLook;
+    [SerializeField] float mouseSensitivity = 1f;
 
     private float verticalRotStore;
     private Vector2 mouseInput;
+
     private Vector3 moveDirection;
     private Vector3 movement;
+    private float activeMoveSpeed;
+
+    private CharacterController characterController;
+    private Camera cam;
 
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        characterController = GetComponent<CharacterController>(); 
+        cam = Camera.main;
     }
 
     private void Update()
@@ -32,6 +44,11 @@ public class PlayerController : MonoBehaviour
         Move();
     }
 
+    private void LateUpdate()
+    {
+        cam.transform.position = viewPoint.position;
+        cam.transform.rotation = viewPoint.rotation;
+    }
 
     private void GetMouseInput()
     {
@@ -50,14 +67,15 @@ public class PlayerController : MonoBehaviour
     }
     private void Move()
     {
-        movement = ((transform.forward * moveDirection.z) + (transform.right * moveDirection.x)).normalized;
-        transform.position += movement * moveSpeed * Time.deltaTime;
+        activeMoveSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : moveSpeed;
+
+        movement = ((transform.forward * moveDirection.z) + (transform.right * moveDirection.x)).normalized * activeMoveSpeed;
+        characterController.Move(movement * Time.deltaTime);
     }
 
     private void RotateHorizontal()
     {
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
-
     }
 
     private void RotateVertical()
